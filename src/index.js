cf91c8a61a7eaa18d98ccf19ebdcfb59795da628
ziho <ziho@kakao.com>
2021-09-12 21:28:39 +0900

    feat: theme 설정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-import App from "./App";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import reset from 'styled-reset';
+import App from './App';
+import { theme } from './theme';
 const GlobalStyles = createGlobalStyle`
   ${reset}
+
+  * {
+    color: #6B7280;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  footer, header {
+    a:hover {
+      color : rgba(17,24,39,1); 
+    }
+  }
+
+  
+
 `;
 
 ReactDOM.render(
-    <React.StrictMode>
-        <GlobalStyles />
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
