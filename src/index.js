66f2854c97b0454b3f6f051cc54a87f68c2b86f7
kimyoungyin <mafa1234@naver.com>
2021-09-15 01:24:42 +0900

    refactor: theme 색 변경 및 GlobalStyle 수정

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import reset from 'styled-reset';
-import App from './App';
-import { theme } from './theme';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import App from "./App";
+import { theme } from "./theme";
 const GlobalStyles = createGlobalStyle`
   ${reset}
 
   * {
-    color: #6B7280;
+    color: ${(props) => props.theme.grey_500};
   }
 
+  body {
+    font-family : "Inter var", ui-sans-serif, system-ui, -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    line-height:1.5rem;
+  }
   a{
     text-decoration: none;
   }
-
-  footer, header {
-    a:hover {
-      color : rgba(17,24,39,1); 
-    }
-  }
-
-  
-
 `;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
