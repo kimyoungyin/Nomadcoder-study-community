efb33764d2bcfdce360eae78ac4ab6f9f584baaa
Joseph <joseph@teamo2.kr>
2021-09-18 22:21:46 +0900

    feat: header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -23,51 +23,17 @@ const Logo = styled.img`
 
 const LogoWrapper = styled.div``;
 
-const MenuWrapper = styled.div`
-  display: none;
-  @media (max-width: 639px) {
-    display: block;
-    svg {
-      width: 2rem;
-      path:nth-child(2) {
-        display: none;
-      }
-    }
-  }
-`;
-
 const ButtonWrapper = styled.div`
   display: flex;
   gap: 1rem;
   align-items: center;
-
-  @media (max-width: 639px) {
-    button:nth-child(1) {
-      display: none;
-    }
-  }
 `;
 
 function Header() {
   return (
     <Container>
       <PaddingWrapper>
-        <MenuWrapper>
-          <svg stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            ></path>
-          </svg>
-        </MenuWrapper>
+        
         <LogoWrapper>
           <Logo src={logo} alt="" />
         </LogoWrapper>
