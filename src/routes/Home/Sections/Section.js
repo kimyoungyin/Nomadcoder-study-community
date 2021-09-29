ff977814c7215d6f84dbbf68541ec16273e663aa
김영인 <yin199859@gmail.com>
2021-09-29 19:37:30 +0900

    refactor: 밀리초 변수(상수)로 치환

--- src/routes/Home/Sections/Section.js
+++ src/routes/Home/Sections/Section.js
@@ -153,16 +153,22 @@ const Section = ({
 
     const calculateTerm = (createdAt) => {
         const gap = Date.now() - createdAt;
-        if (gap >= 604800000) {
-            return `${Math.floor(gap / 604800000)} weeks ago`;
-        } else if (gap >= 86400000) {
-            return `${Math.floor(gap / 86400000)} days ago`;
-        } else if (gap >= 3600000) {
-            return `${Math.floor(gap / 3600000)} hours ago`;
+        const MILLISECOND_PER_MINUTE = 60000;
+        const MILLISECOND_PER_HOUR = MILLISECOND_PER_MINUTE * 60;
+        const MILLISECOND_PER_DAY = MILLISECOND_PER_HOUR * 24;
+        const MILLISECOND_PER_WEEK = MILLISECOND_PER_DAY * 7;
+
+        if (gap >= MILLISECOND_PER_WEEK) {
+            return `${Math.floor(gap / MILLISECOND_PER_WEEK)} weeks ago`;
+        } else if (gap >= MILLISECOND_PER_DAY) {
+            return `${Math.floor(gap / MILLISECOND_PER_DAY)} days ago`;
+        } else if (gap >= MILLISECOND_PER_HOUR) {
+            return `${Math.floor(gap / MILLISECOND_PER_HOUR)} hours ago`;
         } else {
-            return `${Math.floor(gap / 60000)} minutes ago`;
+            return `${Math.floor(gap / MILLISECOND_PER_MINUTE)} minutes ago`;
         }
     };
+
     return (
         <SectionCard as="section" isMain={isPinned} isLiked={isLiked}>
             <div className="section-likeBox" onClick={handleLikeData}>
