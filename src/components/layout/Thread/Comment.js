b19d86f48497ee891be043b26d5e7605c3760a08
김영인 <yin199859@gmail.com>
2021-10-27 15:25:51 +0900

    feat: comment,subComment 레이아웃

--- src/components/layout/Thread/Comment.js
+++ src/components/layout/Thread/Comment.js
@@ -13,41 +13,65 @@ import Card from "../../UI/Card";
 import LikeButton from "../common/LikeButton";
 
 const CommentCard = styled(Card)`
-    width: 100%;
     padding: 0.75rem;
     padding-bottom: 1.25rem;
-    .comment-info {
+    margin-left: ${(props) => props.isSub && "4rem"};
+    .comment-top {
         display: flex;
         align-items: center;
-        .comment-ownerData {
+        justify-content: space-between;
+        .comment-info {
             display: flex;
-            img {
-                width: 2rem;
-                border-radius: 50%;
-                margin-right: 0.75rem;
-            }
-            div {
+            align-items: center;
+            .comment-ownerData {
                 display: flex;
                 align-items: center;
-                line-height: 1.5;
-                .comment-ownerName {
-                    font-weight: 600;
-                    font-size: 0.875rem;
+                img {
+                    width: 2rem;
+                    height: 2rem;
+                    border-radius: 50%;
+                    margin-right: 0.75rem;
                 }
-                .comment-createdAt {
-                    font-size: 0.75rem;
+                div {
+                    display: flex;
+                    align-items: center;
+                    line-height: 1.5;
+                    .comment-ownerName {
+                        font-weight: 600;
+                        font-size: 0.875rem;
+                    }
+                    .comment-createdAt {
+                        font-size: 0.75rem;
+                    }
+                }
+            }
+        }
+        .comment-icons {
+            display: flex;
+            & > div {
+                width: 1rem;
+                margin-right: 0.75rem;
+                cursor: pointer;
+                & > svg > path {
+                    color: black;
                 }
             }
         }
     }
+    .comment-text {
+        margin-top: 1rem;
+        margin-left: ${(props) => props.isSub || "4rem"};
+        color: black;
+    }
 `;
 
-const Comment = ({ commentObj, displayName, threadId }) => {
+const Comment = ({ commentObj, displayName, threadId, isSub }) => {
     const [isLiked, setIsLiked] = useState(
         commentObj.likes.includes(displayName)
     );
     const [likedNumber, setLikedNumber] = useState(commentObj.likes.length);
     const history = useHistory();
+    const term = useTerm(commentObj.createdAt);
     const commentRef = doc(
         db,
         "threads",
@@ -91,29 +115,62 @@ const Comment = ({ commentObj, displayName, threadId }) => {
     };
 
     return (
-        <CommentCard>
-            <div className="comment-info">
-                <LikeButton
-                    onLikeTransaction={checkLikeStateAndRunTransaction}
-                    likedNumber={likedNumber}
-                    isLiked={isLiked}
-                />
-                <div className="comment-ownerData">
-                    <img
-                        src={commentObj.owner.photoURL}
-                        alt={commentObj.owner.displayName}
-                    />
-                    <div>
-                        <span className="comment-ownerName">
-                            {commentObj.owner.displayName}
-                        </span>
-                        <span className="comment-createdAt">
-                            &nbsp;|&nbsp;{useTerm(commentObj.createdAt)}
-                        </span>
+        <CommentCard isSub={isSub}>
+            <div className="comment-top">
+                <div className="comment-info">
+                    {isSub || (
+                        <LikeButton
+                            onLikeTransaction={checkLikeStateAndRunTransaction}
+                            likedNumber={likedNumber}
+                            isLiked={isLiked}
+                        />
+                    )}
+                    <div className="comment-ownerData">
+                        <img
+                            src={commentObj.owner.photoURL}
+                            alt={commentObj.owner.displayName}
+                        />
+                        <div>
+                            <span className="comment-ownerName">
+                                {commentObj.owner.displayName}
+                            </span>
+                            <span className="comment-createdAt">
+                                &nbsp;|&nbsp;{term}
+                            </span>
+                        </div>
                     </div>
                 </div>
+                <div className="comment-icons">
+                    <div className="comment-makeSub" onClick={() => {}}>
+                        <svg fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                clip-rule="evenodd"
+                                fill-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </div>
+                    {commentObj.owner.displayName === displayName && (
+                        <>
+                            <div className="comment-edit">
+                                <svg fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                                </svg>
+                            </div>
+                            <div className="comment-delte">
+                                <svg fill="currentColor" viewBox="0 0 20 20">
+                                    <path
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"
+                                        fill-rule="evenodd"
+                                    ></path>
+                                </svg>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
-            <div>{commentObj.comment}</div>
+            <div className="comment-text">{commentObj.comment}</div>
         </CommentCard>
     );
 };
