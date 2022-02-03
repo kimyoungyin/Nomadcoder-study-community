4eb895861081ebf0dc8df3ca5e4cf1bbf8dab3a5
kimyoungyin <yin199859@gmail.com>
2022-02-04 01:45:55 +0900

    fix: app 전체 loading중일 때 loader 컴포넌트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import Router from './router';
-import Footer from './components/layout/Footer';
-import { useSetRecoilState } from 'recoil';
-import { authState, isLoggedInState } from './recoil/authRecoil';
-import { useEffect, useState } from 'react';
-import { authService } from './fb';
+import Router from "./router";
+import Footer from "./components/layout/Footer";
+import { useSetRecoilState } from "recoil";
+import { authState, isLoggedInState } from "./recoil/authRecoil";
+import { useEffect, useState } from "react";
+import { authService } from "./fb";
+import Loader from "./components/Loader";
 
 function App() {
-  const setAuth = useSetRecoilState(authState);
-  const setIsLoggedIn = useSetRecoilState(isLoggedInState);
-  const [isLoading, setIsLoading] = useState(true);
+    const setAuth = useSetRecoilState(authState);
+    const setIsLoggedIn = useSetRecoilState(isLoggedInState);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const unsubscribe = authService.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        setAuth(authUser);
-        setIsLoggedIn(true);
-      }
-      setIsLoading(false);
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+    useEffect(() => {
+        const unsubscribe = authService.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                setAuth(authUser);
+                setIsLoggedIn(true);
+            }
+            setIsLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
-  return (
-    <div className="App">
-      {isLoading ? <p>Loading..</p> : <Router />}
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            {isLoading ? <Loader width={4} /> : <Router />}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
